bba761c36329ab0ac786ec6e39c58d511e72780a
Alena Bukilic <bukilicalena@gmail.com>
2020-12-12 17:25:40 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+import { filterImageFromURL, deleteLocalFiles, isValidUrl } from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get('/filteredimage',
+    async(req: Request, res: Response) => {
+
+    const { image_url } = req.query;
+
+    if (!image_url) {
+        return res.status(400)
+            .send('image url is required.')
+    }
+
+    if (!isValidUrl(image_url)) {
+        return res.status(400)
+            .send('string provided is not valid url.');
+    }
+
+    const filteredImage = await filterImageFromURL(image_url);
+
+    res.sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
+  });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
